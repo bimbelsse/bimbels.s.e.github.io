cf84183e3497fa22ef7c322eb3a4ede220204063
bimbelsse <angga@sdimohammadhatta.sch.id>
2023-10-13 23:10:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+//Klik diluar side bar untuk menghilangkan menu
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
